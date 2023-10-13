a41abb88c563fe30592f8b3dfac6a2b4e9fe06e5
Ruben <rubend.alvarezc@gmail.com>
2023-10-12 23:23:00 -0400

    db queries updated

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,8 @@ CREATE TABLE roles (
   title VARCHAR(30), 
   salary DECIMAL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id) 
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id) 
 );
 
 CREATE TABLE employee (
@@ -22,7 +23,9 @@ CREATE TABLE employee (
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) 
+  REFERENCES roles(id),
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id)
 );
 
